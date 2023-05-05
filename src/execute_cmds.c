3a717c41b5e192eddfc5eda8a47dc1858fca5571
Flip Van Wijk <fvan-wij@f0r4s18.codam.nl>
2023-05-05 17:53:02 +0200

    Can now run more than 2 process + pipes

--- src/execute_cmds.c
+++ src/execute_cmds.c
@@ -6,45 +6,84 @@
 /*   By: flip <marvin@42.fr>                          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/02 23:55:30 by flip          #+#    #+#                 */
-/*   Updated: 2023/05/04 18:23:27 by fvan-wij      ########   odam.nl         */
+/*   Updated: 2023/05/05 17:50:10 by fvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
+void	*find_cmd(t_cmd *cmd_list, t_cmd **node, int process_count)
+{
+	*node = cmd_list;
+	while (node != NULL)
+	{
+		if ((*node)->cmd_index == process_count)
+		{
+			printf("node->cmd_index: %d\tprocess_count: %d\n", (*node)->cmd_index, process_count);
+			return (node);
+		}
+		*node = (*node)->next;
+	}
+	return (perror("Process couldn't match cmd_id"), NULL);
+}
+
+int	run_initial_child_process(t_cmd *cmd_node, int pipe_fd[], char *envp[])
+{
+	int	error_code;
+	int	infile;
+
+	printf("first cmd [%s]\n", cmd_node->cmds[0]);
+	close(pipe_fd[0]);
+	infile = open("infile", O_RDONLY);
+	dup2(infile, STDIN_FILENO);
+	dup2(pipe_fd[1], STDOUT_FILENO);
+	error_code = execve(cmd_node->cmd_path, cmd_node->cmds, envp);
+	return (error_code);
+}
+
+int	run_child_process(t_cmd *cmd_node, int pipe_fd[], char *envp[])
+{
+	int error_code;
+	
+	printf("middle cmd [%s]\n", cmd_node->cmds[0]);
+	dup2(pipe_fd[0], STDIN_FILENO);
+	dup2(pipe_fd[1], STDOUT_FILENO);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	error_code = execve(cmd_node->cmd_path, cmd_node->cmds, envp);
+	return (error_code);
+}
+
+int run_final_child_process(t_cmd *cmd_node, int pipe_fd[], char *envp[])
+{
+	int error_code;
+	int	outfile;
+
+	printf("last cmd [%s]\n", cmd_node->cmds[0]);
+	close(pipe_fd[1]);
+	dup2(pipe_fd[0], STDIN_FILENO);
+	outfile = open("outfile", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	dup2(outfile, STDOUT_FILENO);
+	error_code = execve(cmd_node->cmd_path, cmd_node->cmds, envp);
+	return (error_code);
+}
+
 int	execute_cmd(t_pipex *meta, int process_count, char *envp[], int pipe_fd[])
 {
-	t_cmd	*node;
+	t_cmd	*cmd_node;
 	int		error_code;
-	int		i;
-	int		file;
 
-	i = 0;
-	error_code = 0;
-	node = meta->cmd_list;
-	while (node->cmd_index != process_count && node->next != NULL)
-		node = node->next;
-	if (node->cmd_index != process_count)
-		return (perror("Process couldn't match cmd_id."), 0);
+	error_code = -1;
+	if (!find_cmd(meta->cmd_list, &cmd_node, process_count))
+		return (error_code);
+	if (!cmd_node)
+		return (perror("Could not find cmd in cmd_list"), 2);
 	if (process_count == 0)
-	{
-		printf("first cmd [%s]\n", node->cmds[0]);
-		close(pipe_fd[0]);
-		file = open("infile", O_RDONLY);
-		dup2(file, STDIN_FILENO);
-		dup2(pipe_fd[1], STDOUT_FILENO);
-		error_code = execve(node->cmd_path, node->cmds, envp);
-	}
-	else if (process_count == meta->cmd_count - 1)
-	{
-		printf("last cmd [%s]\n", node->cmds[0]);
-		close(pipe_fd[1]);
-		dup2(pipe_fd[0], STDIN_FILENO);
-		file = open("outfile", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		dup2(file, STDOUT_FILENO);
-		error_code = execve(node->cmd_path, node->cmds, envp);
-	}
-	//To do: Implement pipes for commands that aren't the first or the last.
+		error_code = run_initial_child_process(cmd_node, pipe_fd, envp);
+	else if (process_count != 0 && process_count != meta->cmd_count -1)
+		error_code = run_child_process(cmd_node, pipe_fd, envp);		
+	else 
+		error_code = run_final_child_process(cmd_node, pipe_fd, envp);
 	printf("error_code: %d\n", error_code);
 	return (error_code);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: flip <marvin@42.fr>                          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/22 00:03:38 by flip          #+#    #+#                 */
-/*   Updated: 2023/05/04 18:14:21 by fvan-wij      ########   odam.nl         */
+/*   Updated: 2023/05/05 17:36:44 by fvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,25 @@ void	print_cmds(t_pipex **meta)
 	}
 }
 
+int	(*initialize_pipes(int n))[2]
+{
+	int (*pipe_array)[2] = malloc(n * sizeof(int[2]));
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		pipe(pipe_array[i]);
+		i++;
+	}
+	return pipe_array;
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
 	t_pipex *meta;
 	pid_t	pid;
-	int		pipe_fd[2];
+	int		(*pipe_array)[2];
 	int		execution_status;
 
 	meta = malloc(sizeof(t_pipex) * 1);
@@ -56,14 +70,14 @@ int main(int argc, char *argv[], char *envp[])
 		write(1, "PARSING_STATUS: Incorrect argc.\n\n", 33);
 		exit(1);
 	}
-	pipe(pipe_fd); 
+	pipe_array = initialize_pipes(meta->cmd_count - 1);
 	pid = 1;
 	while (meta->process_count < meta->cmd_count - 1 && pid != 0)
 	{
 		meta->process_count++;
 		pid = fork();
 		if (pid == 0)
-			execution_status = execute_cmd(meta, meta->process_count, envp, pipe_fd);
+			execution_status = execute_cmd(meta, meta->process_count, envp, pipe_array[0]);
 	}
 	return (execution_status);
 }
